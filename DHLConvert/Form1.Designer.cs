af0f39094b3a7a091af3780a9ff8803525b46c72
vulture20 <vulture20@web.de>
2015-12-22 15:06:00 +0100

    Initial commit

---
+++ DHLConvert/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace DHLConvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dateiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.druckenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pDFSpeichernToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.beendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.überToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pDFÖffnenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "&Drucken";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(850, 349);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(104, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "&PDF speichern";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dateiToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(850, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dateiToolStripMenuItem
+            // 
+            this.dateiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pDFÖffnenToolStripMenuItem,
+            this.pDFSpeichernToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.druckenToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.beendenToolStripMenuItem});
+            this.dateiToolStripMenuItem.Name = "dateiToolStripMenuItem";
+            this.dateiToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.dateiToolStripMenuItem.Text = "Datei";
+            // 
+            // druckenToolStripMenuItem
+            // 
+            this.druckenToolStripMenuItem.Enabled = false;
+            this.druckenToolStripMenuItem.Name = "druckenToolStripMenuItem";
+            this.druckenToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.druckenToolStripMenuItem.Text = "&Drucken";
+            this.druckenToolStripMenuItem.Click += new System.EventHandler(this.druckenToolStripMenuItem_Click);
+            // 
+            // pDFSpeichernToolStripMenuItem
+            // 
+            this.pDFSpeichernToolStripMenuItem.Enabled = false;
+            this.pDFSpeichernToolStripMenuItem.Name = "pDFSpeichernToolStripMenuItem";
+            this.pDFSpeichernToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pDFSpeichernToolStripMenuItem.Text = "&PDF speichern";
+            this.pDFSpeichernToolStripMenuItem.Click += new System.EventHandler(this.pDFSpeichernToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // beendenToolStripMenuItem
+            // 
+            this.beendenToolStripMenuItem.Name = "beendenToolStripMenuItem";
+            this.beendenToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.beendenToolStripMenuItem.Text = "&Beenden";
+            this.beendenToolStripMenuItem.Click += new System.EventHandler(this.beendenToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.überToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(24, 20);
+            this.toolStripMenuItem1.Text = "&?";
+            // 
+            // überToolStripMenuItem
+            // 
+            this.überToolStripMenuItem.Name = "überToolStripMenuItem";
+            this.überToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.überToolStripMenuItem.Text = "Über";
+            this.überToolStripMenuItem.Click += new System.EventHandler(this.überToolStripMenuItem_Click);
+            // 
+            // pDFÖffnenToolStripMenuItem
+            // 
+            this.pDFÖffnenToolStripMenuItem.Name = "pDFÖffnenToolStripMenuItem";
+            this.pDFÖffnenToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pDFÖffnenToolStripMenuItem.Text = "PDF ö&ffnen";
+            this.pDFÖffnenToolStripMenuItem.Click += new System.EventHandler(this.pDFÖffnenToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "PDF-Paketmarken|*.pdf|Alle Dateien|*.*";
+            this.openFileDialog1.Title = "Paketmarken-PDF öffnen";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "pdf";
+            this.saveFileDialog1.FileName = "DHL.pdf";
+            this.saveFileDialog1.Filter = "PDF-Dateien|*.pdf|Alle Dateien|*.*";
+            this.saveFileDialog1.Title = "Label als PDF speichern";
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.AllowPrintToFile = false;
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.DocumentName = "DHL-Label";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 417);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DHLConvert";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dateiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem druckenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pDFSpeichernToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem beendenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem überToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pDFÖffnenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ DHLConvert/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Pdf;
+
+namespace DHLConvert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Bitmap getClip(Image source, Rectangle rect)
+        {
+            Bitmap tempBitmap = new Bitmap(rect.Width, rect.Height);
+
+            using (Graphics g = Graphics.FromImage(tempBitmap))
+            {
+                g.DrawImage(source, new Rectangle(0, 0, tempBitmap.Width, tempBitmap.Height), rect, GraphicsUnit.Pixel);
+            }
+
+            return tempBitmap;
+        }
+
+        private Bitmap convertPDF(string filename)
+        {
+            PdfDocument doc = new PdfDocument();
+            Image source;
+            Rectangle rectMain = new Rectangle(1662, 171, 1142, 685);
+            Rectangle rectBarcode = new Rectangle(1803, 1240, 556, 685);
+            Rectangle rectTracking = new Rectangle(1665, 857, 749, 113);
+            Bitmap bitmapMain = new Bitmap(rectMain.Width, rectMain.Height);
+            Bitmap bitmapTracking = new Bitmap(rectTracking.Width, rectTracking.Height);
+            Bitmap bitmapBarcode = new Bitmap(rectBarcode.Width, rectBarcode.Height);
+            Bitmap bitmapLabel = new Bitmap(1701, 697);
+
+            try
+            {
+                doc.LoadFromFile(filename);
+                source = doc.SaveAsImage(0, 273, 273);
+                source.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                bitmapMain = getClip(source, rectMain);
+                bitmapTracking = getClip(source, rectTracking);
+
+                source = doc.SaveAsImage(0, 255, 255);
+                source.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                bitmapBarcode = getClip(source, rectBarcode);
+
+                using (Graphics g = Graphics.FromImage(bitmapLabel))
+                {
+                    g.DrawImage(bitmapMain, 0, 0);
+                    g.DrawImage(bitmapTracking, 3, 570);
+                    g.DrawImage(bitmapBarcode, 1142, 0);
+                }
+
+                return bitmapLabel;
+            }
+            catch (Spire.Pdf.Exceptions.PdfDocumentException) {
+                return null;
+            }
+        }
+
+        private bool savePDF(string filename)
+        {
+            try
+            {
+                PdfDocument doc = new PdfDocument();
+                PdfSection section = doc.Sections.Add();
+                PdfPageBase page = doc.Pages.Add(new SizeF(408.32f, 167.3f), new Spire.Pdf.Graphics.PdfMargins(0));
+//            PdfPageBase page = doc.Pages.Add(new SizeF(150f, 62f), new Spire.Pdf.Graphics.PdfMargins(0));
+                Spire.Pdf.Graphics.PdfImage image = Spire.Pdf.Graphics.PdfImage.FromImage(pictureBox1.Image);
+                float widthFitRate = image.PhysicalDimension.Width / page.Canvas.ClientSize.Width;
+                float heightFitRate = image.PhysicalDimension.Height / page.Canvas.ClientSize.Height;
+                float fitRate = Math.Max(widthFitRate, heightFitRate);
+                float fitWidth = image.PhysicalDimension.Width / fitRate;
+                float fitHeight = image.PhysicalDimension.Height / fitRate;
+
+                doc.DocumentInformation.Author = "(C) 2015 by Thorsten Schröpel";
+                doc.DocumentInformation.Title = "DHL-Paketmarke";
+                doc.DocumentInformation.Producer = "DHLConvert";
+                doc.DocumentInformation.Keywords = "DHLConvert DHL Paketmarke Label Aufkleber";
+                page.Canvas.DrawImage(image, 0.3f, 0.5f, fitWidth, fitHeight);
+                doc.SaveToFile(filename);
+//            bitmapLabel.Save("dhl.png", System.Drawing.Imaging.ImageFormat.Png);
+                doc.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void enableControls()
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            pDFSpeichernToolStripMenuItem.Enabled = true;
+            druckenToolStripMenuItem.Enabled = true;
+        }
+
+        private void printLabel(Image label)
+        {
+            printDocument1.DefaultPageSettings.Landscape = true;
+            printDocument1.PrintPage += (sender, args) =>
+            {
+                Rectangle m = args.MarginBounds;
+
+                if ((double)label.Width / (double)label.Height > (double)m.Width / (double)m.Height) 
+                {
+                    m.Height = (int)((double)label.Height / (double)label.Width * (double)m.Width);
+                }
+                else
+                {
+                    m.Width = (int)((double)label.Width / (double)label.Height * (double)m.Height);
+                }
+
+                args.Graphics.DrawImage(pictureBox1.Image, m);
+            };
+            printDocument1.Print();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (printDialog1.ShowDialog() == DialogResult.OK)
+            {
+                printLabel(pictureBox1.Image);
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            Bitmap bitmapPDF;
+
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            bitmapPDF = convertPDF(files[0]);
+            if (bitmapPDF == null)
+            {
+                MessageBox.Show("Keine gültige Paketmarke!");
+            }
+            else
+            {
+                pictureBox1.Image = bitmapPDF;
+                enableControls();
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void beendenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool userClickedOK = (saveFileDialog1.ShowDialog() == DialogResult.OK);
+
+            if (userClickedOK == true)
+            {
+                if (savePDF(saveFileDialog1.FileName) == false)
+                {
+                    MessageBox.Show("Das DHL-Label konnte nicht gespeichert werden!");
+                }
+            }
+        }
+
+        private void überToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 aboutBox = new AboutBox1();
+
+            aboutBox.ShowDialog();
+        }
+
+        private void druckenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (printDialog1.ShowDialog() == DialogResult.OK)
+            {
+                printLabel(pictureBox1.Image);
+            }
+        }
+
+        private void pDFSpeichernToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool userClickedOK = (saveFileDialog1.ShowDialog() == DialogResult.OK);
+
+            if (userClickedOK == true)
+            {
+                if (savePDF(saveFileDialog1.FileName) == false)
+                {
+                    MessageBox.Show("Das DHL-Label konnte nicht gespeichert werden!");
+                }
+            }
+        }
+
+        private void pDFÖffnenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmapPDF;
+
+            bool userClickedOK = (openFileDialog1.ShowDialog() == DialogResult.OK);
+
+            if (userClickedOK == true)
+            {
+                bitmapPDF = convertPDF(openFileDialog1.FileName);
+                if (bitmapPDF == null) {
+                    MessageBox.Show("Keine gültige Paketmarke!");
+                } else {
+                    pictureBox1.Image = bitmapPDF;
+                    enableControls();
+                }
+            }
+        }
+    }
+}
